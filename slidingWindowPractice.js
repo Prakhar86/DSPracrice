aaea12dbc145c34006c0c8095501139ea2503894
Prakhar86 <prakhar.ag76@gmail.com>
2026-06-01 16:37:01 +0530

    Update slidingWindowPractice.js

--- slidingWindowPractice.js
+++ slidingWindowPractice.js
@@ -13,3 +13,21 @@ var findMaxAverage = function(nums, k) {
 };
 
  console.log(findMaxAverage([1,12,-5,-6,50,3], 4));
+
+
+var minSubArrayLen = function(target, nums) {
+    let sum = 0;
+    let ans = Number.MAX_VALUE;
+    let left = 0;
+    for(let i = left; i<nums.length; i++) {
+        sum =  sum + nums[i];
+        while(sum>=target) {
+            ans = Math.min(ans, i-left+1);
+            sum = sum - nums[left];
+            left++;
+        }
+    }
+    return ans === Number.MAX_VALUE? 0: ans;
+    
+    
+};
