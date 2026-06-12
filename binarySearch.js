392b157aede78d30923f56109d19920a53be6b6a
Prakhar86 <prakhar.ag76@gmail.com>
2026-06-12 13:14:07 +0530

    Update binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -20,3 +20,32 @@ var minEatingSpeed = function (piles, h) {
     }
     return ans;
 };
+
+
+var shipWithinDays = function (weights, days) {
+ 
+  let start = Math.max(...weights);
+  let end = weights.reduce((accumulator, current) => accumulator + current, 0);
+  let ans = 0;
+  while (start <= end) {
+     let sum = 0;
+    let daysInloop = 1;
+    let mid = Math.floor((start + end) / 2);
+    for (let i = 0; i < weights.length; i++) {
+      if (sum + weights[i] > mid) {
+        daysInloop++;
+        sum = 0;
+      }
+      sum = sum + weights[i];
+    }
+
+    if (daysInloop <= days) {
+      ans = mid;
+      end = mid - 1
+    } else {
+      start = mid + 1
+    }
+  }
+  
+  return ans;
+};
